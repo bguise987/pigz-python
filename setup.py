6b0c5c2832011ac8085ff3e38a9488b740156a97
nix7drummer88 <bguise135@gmail.com>
2020-03-27 02:17:00 -0400

    #1: Rename module, format setup.py to conform to Black

--- setup.py
+++ setup.py
@@ -3,32 +3,32 @@ from os import path
 
 from setuptools import find_packages, setup
 
-from pigz-python import __version__
+from pigz_python import __version__
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-    name='pigz-python',
+    name="pigz-python",
     version=__version__,
-    author='Ben Guise',
-    author_email='bguise135@gmail.com',
-    maintainer='Ben Guise',
-    maintainer_email='bguise135@gmail.com',
-    description='A pure Python implementation of the pigz utility.',
-    license='MIT',
-    keywords='zip gzip compression',
-    url='https://github.com/nix7drummer88/pigz-python',
+    author="Ben Guise",
+    author_email="bguise135@gmail.com",
+    maintainer="Ben Guise",
+    maintainer_email="bguise135@gmail.com",
+    description="A pure Python implementation of the pigz utility.",
+    license="MIT",
+    keywords="zip gzip compression",
+    url="https://github.com/nix7drummer88/pigz-python",
     packages=find_packages(),
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Topic :: Utilities'
-    ]
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Utilities",
+    ],
 )
