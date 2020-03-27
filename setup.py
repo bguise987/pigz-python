35df639fc09ce54bc7706ebceec5210889376ee2
nix7drummer88 <bguise135@gmail.com>
2020-03-27 01:39:09 -0400

    Fix import sort order

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """`pigz-python` lives on `GitHub <https://github.com/nix7drummer88/pigz-python>`_."""
-from setuptools import find_packages, setup
 from os import path
+from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
