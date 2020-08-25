4f419db83f54df5783251408a68afb9e91d0d3b0
nix7drummer88 <bguise135@gmail.com>
2020-08-25 00:44:25 -0400

    #13: Add more unit tests for determine XFL

--- tests/test_pigz_python.py
+++ tests/test_pigz_python.py
@@ -18,3 +18,22 @@ class TestPigzPython(unittest.TestCase):
                 value
             )
             self.assertEqual(xfl, expected_xfl)
+
+    def test_determine_extra_flags_min_compression(self):
+        """ Ensure appropriate XFL or eXtra FLags value is returned for min compression value """
+        compression_level = 1
+        expected_xfl = 4
+        xfl = pigz_python.PigzFile._determine_extra_flags(  # pylint: disable=protected-access
+            compression_level
+        )
+        self.assertEqual(xfl, expected_xfl)
+
+    def test_determine_extra_flags_default_compression(self):
+        """ Ensure appropriate XFL or eXtra FLags value is returned for values not max or min """
+        compression_levels = [0, 2, 3, 4, 5, 6, 7, 8]
+        expected_xfl = 0
+        for value in compression_levels:
+            xfl = pigz_python.PigzFile._determine_extra_flags(  # pylint: disable=protected-access
+                value
+            )
+            self.assertEqual(xfl, expected_xfl)
