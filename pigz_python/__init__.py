61b780111f1618a64db6d4dd819489a94f797a43
bguise987 <bguise135@gmail.com>
2026-04-02 15:43:20 -0400

    Black reformat __init__.py

--- pigz_python/__init__.py
+++ pigz_python/__init__.py
@@ -1,5 +1,7 @@
-""" Metadata about the Pigz Python package """
+"""Metadata about the Pigz Python package"""
+
 from importlib.metadata import version
+
 __version__ = version("pigz-python")
 
 from pigz_python.pigz_python import PigzFile, compress_file  # noqa
